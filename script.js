45b734200599729457e4ee42387bef818c590337
Muhammad Farouq <muhammadfarouq225@gmail.com>
2024-06-14 20:47:25 +0700

    sidebar look update
    re coloring and making slight improvement

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "200px";
+  document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "-100px";
 }
 
